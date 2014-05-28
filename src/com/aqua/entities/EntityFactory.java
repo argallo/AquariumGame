3212b8914c39a0f709a4e680f29ce2d4252c2308
argallo <argallo@wpi.edu>
2014-05-28 01:35:08 -0400

    added entity factory

--- src/com/aqua/entities/EntityFactory.java
+++ src/com/aqua/entities/EntityFactory.java
@@ -4,15 +4,25 @@ import com.aqua.GameManager;
 import com.aqua.entities.fish.SimpleFish;
 import com.aqua.gamecomponents.GameView;
 
+/**
+ * 
+ * @author Gallo
+ *
+ */
 public class EntityFactory {
-
+	
 	GameManager gameManager = GameManager.getInstance();
-
+	
 	public EntityFactory() {
 		
 	}
 
-	
+	/**
+	 * 
+	 * @param entityName name to take into factory which determines which entity is created
+	 * @param g the gameview that will be used in each entity
+	 * @return a new entity with a unique id 
+	 */
 	public Entity createEntity(String entityName, GameView g){
 		if(entityName.equals("simplefish")){
 			return new SimpleFish(g, gameManager.newEntityId());
